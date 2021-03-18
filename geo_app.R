c7fd05bc356c3dfc5f65abbff79c3b79553b3f91
Aleksandra Lubicka <48988356+alubiss@users.noreply.github.com>
2021-03-18 21:25:18 +0100

    app core

--- geo_app.R
+++ geo_app.R
@@ -7,42 +7,18 @@ theme_set(theme_bw())
 library("sf")
 library(shinydashboard)
 library(shinyjs)
+#dashboardPage(skin = "black")
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- dashboardPage(
+    skin = "purple",
+    dashboardHeader(title = "GEO app"),
+    dashboardSidebar(sidebarMenu(
+        menuItem("Dashboard", tabName = "Dashboard"),
+        menuItem("Tables", tabName = "Tables")
+    )),
+    dashboardBody()
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output) { }
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
